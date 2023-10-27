58dd7131b545b42dda9449d56e8fea253005831b
Israfil Baghirov <68228242+IsaacBakerov@users.noreply.github.com>
2023-10-27 19:21:30 +0400

    Update Main.java

--- Rock-Paper-Scissors (Java)/task/src/rockpaperscissors/Main.java
+++ Rock-Paper-Scissors (Java)/task/src/rockpaperscissors/Main.java
@@ -86,55 +86,5 @@ public class Main {
                 System.out.println("Invalid input");
             }
         }
-
-//        while (true) {
-//            List<String> elements = Arrays.asList("paper", "scissors", "rock");
-//
-//            Random random = new Random();
-//            int num = random.nextInt(3);
-//
-//            String computer = elements.get(num);
-//
-//            String input = sc.nextLine();
-//
-//            if (input.equals(computer)) {
-//                System.out.printf("There is a draw (%s)\n", computer);
-//                score += 50;
-//            } else if (input.equals("!exit")) {
-//                System.out.println("Bye!");
-//                System.exit(0);
-//            } else if (input.equals("!rating")) {
-//                System.out.println("Your rating: " + score);
-//            } else if (elements.contains(input)) {
-//                switch (computer) {
-//                    case "paper" -> {
-//                        if (input.equals("scissors")) {
-//                            System.out.printf("Well done. The computer chose %s and failed\n", computer);
-//                            score += 100;
-//                        } else if (input.equals("rock")) {
-//                            System.out.printf("Sorry, but the computer chose %s\n", computer);
-//                        }
-//                    }
-//                    case "scissors" -> {
-//                        if (input.equals("rock")) {
-//                            System.out.printf("Well done. The computer chose %s and failed\n", computer);
-//                            score += 100;
-//                        } else if (input.equals("paper")) {
-//                            System.out.printf("Sorry, but the computer chose %s\n", computer);
-//                        }
-//                    }
-//                    case "rock" -> {
-//                        if (input.equals("paper")) {
-//                            System.out.printf("Well done. The computer chose %s and failed\n", computer);
-//                            score += 100;
-//                        } else if (input.equals("scissors")) {
-//                            System.out.printf("Sorry, but the computer chose %s\n", computer);
-//                        }
-//                    }
-//                }
-//            } else {
-//                System.out.println("Invalid input");
-//            }
-//        }
     }
 }
